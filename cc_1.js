0ebbaf3ee72b53911e5d396613c51f63b66d4f0a
Linh Pham <linhmaipham@usf.edu>
2025-01-26 16:46:35 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,11 @@ var isActive = true;
 console.log("Employee Name:",employeeName, "- Type:", typeof employeeName); 
 console.log("Employee ID:",employeeID, "- Type:", typeof employeeID);
 console.log("Is Active?",isActive, "- Type:", typeof isActive);
+
+//Task 2: Primitive Data Types
+let productName = "Ice cream"; 
+const productPrice = 4.57;
+var isAvailable = true;
+console.log(`Product Name: ${productName} - Type: ${typeof productName}`);
+console.log(`Product Price: $${productPrice} - Type: ${typeof productPrice}`);
+console.log(`Is Available? ${isAvailable} - Type: ${typeof isAvailable}`);
