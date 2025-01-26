a8f619242ab3ad827607f6f50fc9df28e7f69369
Linh Pham <linhmaipham@usf.edu>
2025-01-26 16:45:59 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+//Task 1: Variables
+let employeeName = "Linh";
+const employeeID = 1202;
+var isActive = true; 
+console.log("Employee Name:",employeeName, "- Type:", typeof employeeName); 
+console.log("Employee ID:",employeeID, "- Type:", typeof employeeID);
+console.log("Is Active?",isActive, "- Type:", typeof isActive);
